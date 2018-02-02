9c86a3d996b4794983705aa67859a0d0311a6f33
DavisGilmore <davisgilmore@yahoo.com>
2018-02-02 13:18:18 -0500

    modified piece extraction by edge to run imfill first

--- MATLAB/PuzzlePieceExtraction/processImage.m
+++ MATLAB/PuzzlePieceExtraction/processImage.m
@@ -13,33 +13,43 @@ function [output, pieceCount] = processImage(fileName)
 %     find(boundedPieces==0)
 %     pieceMask(find(boundedPieces > 0)) = 1;
 %     imtool(pieceMask)
-     noiseRemoved = imopen(zeroDir, strel('square',3));
+    [r,c] = size(zeroDir);
+    zeroDir(1:10,:) = 0;
+    zeroDir(r-9:r,:) = 0;
+    zeroDir(:,1:10) = 0;
+    zeroDir(:,c-9:c) = 0;
+     %noiseRemoved = imopen(zeroDir, strel('square',3));
 %     imtool(noiseRemoved)
     
-    pieceImg = noiseRemoved;
+   % pieceImg = noiseRemoved;
 %     iter=100;
 %     for i=1:iter
 %         pieceImg = imclose(pieceImg, strel('square',3));
 %     end
 
-    pieceImg = imfill(pieceImg);
+    pieceImg = imfill(zeroDir);
     
-    imtool(pieceImg);
-    output = pieceImg;
-    
-    pieces = bwlabel(pieceImg);
-
-    sizeThresh = 300;
-
+    cc = bwlabel(pieceImg, 4);
+    group_num = max(max(cc));
+    max_size = 1;
     count = 0;
-    for i=1:max(max(pieces))
-        idx = find(pieces==i);
-        if(size(idx,1) < sizeThresh)
-            pieces(idx) = 0;
+    for i = 1:group_num
+        s = size(find(cc == i));
+        if s(1,1) > max_size
+            max_size = s(1,1);
+        end
+    end
+    for i = 1:group_num
+        s = size(find(cc == i));
+        if s(1,1) * 10 < max_size
+            cc(find(cc == i)) = 0;
         else
             count = count + 1;
         end
     end
+
+    output = label2rgb(cc, @jet, 'k');
+    imtool(output);
     
     pieceCount = count
 end
